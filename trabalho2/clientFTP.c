4207b43fc06e9ec36b5cb133c52913e230b09852
franciscobarbosa <franciscolamares@gmail.com>
2016-12-09 17:43:48 +0000

    clientFTP updated

--- trabalho2/clientFTP.c
+++ trabalho2/clientFTP.c
@@ -1,60 +1,117 @@
 /*      (C)2000 FEUP  */
 
-#include <stdio.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <netdb.h>
-#include <strings.h>
+#include "clientFTP.h"
 
 #define SERVER_PORT 6000
 #define SERVER_ADDR "192.168.28.96"
+#define SIZE 512
 
-int connectSocket(const char* SERVER_ADDR, int SERVER_PORT){
+int connectSocket(const char* ip, int port){
 
 	int	sockfd;
 	struct	sockaddr_in server_addr;
-	char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";  
 	int	bytes;
-	
+
 	/*server address handling*/
 	bzero((char*)&server_addr,sizeof(server_addr));
 	server_addr.sin_family = AF_INET;
-	server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-	server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-    
+	server_addr.sin_addr.s_addr = inet_addr(ip);	/*32 bit Internet address network byte ordered*/
+	server_addr.sin_port = htons(port);		/*server TCP port must be network byte ordered */
+
 	/*open an TCP socket*/
 	if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
     		perror("socket()");
         	exit(0);
     	}
 	/*connect to the server*/
-    	if(connect(sockfd, 
-	           (struct sockaddr *)&server_addr, 
+    	if(connect(sockfd,
+	           (struct sockaddr *)&server_addr,
 		   sizeof(server_addr)) < 0){
         	perror("connect()");
 		exit(0);
 	}
-    	/*send a string to the server*/
-	bytes = write(sockfd, buf, strlen(buf));
-	printf("Bytes escritos %d\n", bytes);
+
 
 	return sockfd;
 }
 
-int ftpConnect(const char* SERVER_ADDR, int SERVER_PORT){
-	int sockfd=connectSocket(SERVER_ADDR,SERVER_PORT);
-	
+int ftpConnect(const char* ip, int port){
+	int sockfd=connectSocket(ip,port);
+
+	if(sockfd<0){
+		printf("error connecting to socket.\n");
+		return -1;
+	}
+	else{
+		printf("connect succesful");
+	}
+	return sockfd;
 }
 
+int ftpDisconnect(int sockfd){
+	if(ftpWrite(sockfd,"QUIT","")!=0){
+		close(sockfd);
+		return 1;
+	}
+	char replyBuf[SIZE];
+	if(ftpRead(sockfd,replyBuf,SIZE)!=0){
+		close(sockfd);
+		return 1;
+	}
+	close(fd);
+	return 0;
+}
 
+int ftpToPassive(int sockfd) {
+	if (ftpWrite(sockfd, "PASV", "") != 0){
+		close(sockfd);
+		return 1;
+	}
+	char replyBuf[SIZE];
 
+	if (ftpRead(sockfd,replyBuf, SIZE) != 0){
+			close(sockfd);
+			return 1;
+	}
 
+	int ip_1, ip_2, ip_3, ip_4, port_1, port_2;
 
+	if ((sscanf(replyBuf, "Going Passive: %u,%u,%u,%u,%u,%u,%u"), &ip1,&ip2,&ip3,&ip4,&port1,&port2) < 0){
+		printf("ERROR");
+		return 1;
+	}
 
+	sprintf(ip, "%u.%u.%u.%u", ip1, ip2, ip3, ip4);
 
+	int port=250*port1+port2;
+	if(connectSocket(&(sockfd),ip,port)!=0){
+		close(sockfd);
+		return -1;
+	}
+	return 0;
+}
 
+int ftpWrite(int sockfd,char * buff,int size){
+	int n;
+	if((n=write(sockfd,buff,size))==size){
+	   	printf("write succeded");
+		return 0;
+	}
+	else
+	return -1;
+}
+
+int ftpRead(int sockfd,char * buff,int size){
+	File *socketPtr;
+	if((socketPtr=fdopen(sockfd,"r"))==NULL){
+		printf("fdopen failed");
+		return -1;
+	}
+	do{
+		memset(buff,0,size);
+			buff=fgets(buff,size,socketptr);
+		}while(!(buff[0]>='1' && buff[0]<='5'));
+	}
+
+	return 0;
+}
